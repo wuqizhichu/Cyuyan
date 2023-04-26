6d62170574252738e6dbb8d5d257298708393559
wuqizhichu <129579355+wuqizhichu@users.noreply.github.com>
2023-04-26 19:35:28 +0800

    Add files via upload

---
+++ test4.26.c
@@ -0,0 +1,74 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//int main()
+//{
+//	printf("1.中文\n");
+//	printf("2.英文\n");
+//	printf("3.日文\n");
+//	printf("4.法文\n");
+//	printf("请输入你的选择：\n");
+//	return 0;
+//}
+
+
+//输出图形
+
+//int main()
+//{
+//	printf("    **    \n");
+//	printf("   *  *   \n");
+//	printf("  *    *  \n");
+//	printf("*        *\n");
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char a = ' ';
+//	char b = '*';
+//	printf("%c%c%c%c%c%c%c%c\n",a,a,a,b,b,a,a,a);
+//	printf("%c%c%c%c%c%c%c%c\n",a,a,b,a,a,b,a,a); 
+//	printf("%c%c%c%c%c%c%c%c\n",a,b,a,a,a,a,b,a);
+//	printf("%c%c%c%c%c%c%c%c\n",b,a,a,a,a,a,a,b);
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int j = 6;
+//	int i=j += j -= j*j;//-60
+//	printf("%d", i);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	printf("请输入两个整数：\n");
+//	scanf("%d%d", &i, &j);
+//	/*printf("%d %d", i, j);*/
+//	printf("%d+%d=%d\n", i, j, i + j);
+//	printf("%d-%d=%d\n", i, j, i - j);
+//	printf("%d*%d=%d\n", i, j, i * j);
+//	printf("%d/%d=%d\n", i, j, i / j);
+//	printf("%d%%%d=%d\n", i, j, i % j);
+//	return 0;
+//}
+
+//int main()
+//{
+//	float i = 0.0;
+//	float j = 0.0;
+//	printf("请输入两个浮点数：\n");
+//	scanf("%f%f", &i, &j);
+//	/*printf("%d %d", i, j);*/
+//	printf("%f+%f=%f\n", i, j, i + j);
+//	printf("%f-%f=%f\n", i, j, i - j);
+//	printf("%f*%f=%f\n", i, j, i * j);
+//	printf("%f/%f=%f\n", i, j, i / j);
+//	return 0;
+//}
