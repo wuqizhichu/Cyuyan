0efbcbaaf147a1819382bdfe278843bf14790f3a
wuqizhichu <129579355+wuqizhichu@users.noreply.github.com>
2023-05-09 20:28:52 +0800

    Add files via upload

---
+++ test5.9.c
@@ -0,0 +1,114 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//写一个函数求一个数二进制（补码）中1的个数
+//int count_bit_one(unsigned int n)
+//{
+//	int count = 0;
+//	while (n)
+//	{
+//		if (n % 2 == 1)
+//		{
+//			count++;
+//		}
+//		n = n / 2;
+//	}
+//	return count;
+//}
+
+//int count_bit_one(unsigned int n)
+//{
+//	int count = 0;
+//	int i = 0;
+//	for (i = 0; i < 32; i++)
+//	{
+//		if (((n << i) & 1) == 1)
+//		{
+//			count++;
+//		}
+//	}
+//	return count;
+//}
+//
+//int count_bit_one(unsigned int n)
+//{
+//	int count = 0;
+//	while (n)
+//	{
+//		n = n & (n - 1);
+//		count++;
+//	}
+//	return count;
+//}
+//
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d",&a);
+//	int count = count_bit_one(a);
+//	printf("count = %d\n", count);
+//	return 0;
+//}
+
+//int get_diff_bit(int m,int n)
+//{
+//	int tmp = m ^ n;
+//	int count = 0;
+//	//return count_bit_one(tmp);
+//	while (tmp)
+//	{
+//		tmp = tmp & (tmp - 1);
+//		count++;
+//	}
+//}
+//
+//int main()
+//{
+//	int m = 0;
+//	int n = 0;
+//	scanf("%d%d", &m, &n);
+//	int count = get_diff_bit(m, n);
+//	printf("count = %d\n",count);
+//	return 0;
+//}
+
+//void print(int m)
+//{
+//	int i = 0;
+//	printf("奇数位：\n");
+//	for (i = 30; i >= 0; i -= 2)
+//	{
+//		printf("%d", (m >> i) & 1);
+//	}
+//	printf("\n");
+//	printf("偶数位：\n");
+//	for (i = 31; i > 0; i -= 2)
+//	{
+//		printf("%d", (m >> i) & 1);
+//	}
+//	printf("\n");
+//}
+//
+//int main()
+//{
+//	int m = 0;
+//	scanf("%d", &m);
+//	print(m);
+//	return 0;
+//}
+
+void print(int* p, int sz)
+{
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", *(p + i));
+	}
+}
+
+int main()
+{
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	print(arr, sz);
+	return 0;
+}
